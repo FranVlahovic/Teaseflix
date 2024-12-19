933ee7c8ab8475385c9fe8ac07e784f767d3c4bb
FranVlahovic <franvlahovic@proton.me>
2024-12-20 00:02:51 +0100

    Add Icons & Images and Update JS, HTML, CSS Added New Icons for the Dropdown Menu. Added New Images for Profile Avatars inside Dropdown. Added Dropdown logic inside JS (Opening and Closing Dropdown Menu). Added Dropdown menu inside HTML. Stylized Dropdown Menu and Header.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './styles.css';
+
+const dropDownBtn = document.querySelector('.profile');
+const dropDownIcon = document.querySelector('.settings-btn');
+const dropDownMenu = document.querySelector('.dropdown');
+
+const showDropdown = () => {
+    dropDownMenu.classList.add('show');
+    dropDownIcon.style.transform = 'rotate(180deg)';
+};
+
+const hideDropdown = () => {
+    dropDownMenu.classList.remove('show');
+    dropDownIcon.style.transform = 'rotate(0deg)';
+};
+
+const openDropdown = () => {
+    dropDownBtn.addEventListener('mouseenter', () => {
+        showDropdown();
+    });
+    dropDownMenu.addEventListener('mouseenter', () => {
+        showDropdown();
+    });
+};
+
+const closeDropdown = () => {
+    dropDownBtn.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            if (
+                !dropDownBtn.matches(':hover') &&
+                !dropDownMenu.matches(':hover')
+            ) {
+                hideDropdown();
+            }
+        }, 500);
+    });
+    dropDownMenu.addEventListener('mouseleave', () => {
+        hideDropdown();
+    });
+};
+
+openDropdown();
+closeDropdown();
